5c1e41d2bbf1358308b4b88ce4d170c5c1adcf60
Chamod AK <chamodse@gmail.com>
2018-09-05 10:12:46 +0530

    Initialize Website
    Basics of home page, header, footer

---
+++ application/views/header.php
@@ -0,0 +1,81 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title> Siyoth.lk </title>
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
+        <img src="<?php echo base_url() ?>asset/images/logo_white.jpg" width="30" height="30" class="rounded-circle" alt="image">
+        Siyoth.lk
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Bird WiKi</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">News & Articles</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Forum</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Events</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Sanctuaries</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Maps
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Pic Map</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Distribution Maps</a>
+                    <a class="dropdown-item" href="#">Sanctuary Maps</a>
+                </div>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        <ul class="navbar-nav navbar-right mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#"> Login <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    USER
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Profile</a>
+                    <a class="dropdown-item" href="#">Logout</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
---
+++ application/views/home.php
@@ -0,0 +1,20 @@
+<?php include 'header.php' ?>
+
+    <div class="jumbotron ">
+        <h1 class="display-4 text-center"> Welome to Siyoth.lk </h1>
+        <p class="lead text-center"> Explore the beauty of world of birds</p>
+        <hr class="my-4">
+        <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam autem consequatur cupiditate eius hic impedit maiores, officiis vel! Accusamus, adipisci cum dolor eos eum facere ipsum nemo non provident recusandae.</p>
+        <p class="lead d-flex justify-content-center">
+            <a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a>
+        </p>
+    </div>
+
+    <div class="container">
+        <h3 class="text-center"> Contents goes here ... </h3>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius eum expedita illum inventore maxime natus quod ullam. Aperiam deserunt exercitationem fuga magni nemo nobis, non quisquam reiciendis rerum similique soluta?</p>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius eum expedita illum inventore maxime natus quod ullam. Aperiam deserunt exercitationem fuga magni nemo nobis, non quisquam reiciendis rerum similique soluta?</p>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius eum expedita illum inventore maxime natus quod ullam. Aperiam deserunt exercitationem fuga magni nemo nobis, non quisquam reiciendis rerum similique soluta?</p>
+    </div>
+
+<?php include 'footer.php'?>
